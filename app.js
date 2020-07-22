936a361823f07b7e14b55537485d2a5e97640615
Aydin Hepsaydir <arphepsaydir1@sheffield.ac.uk>
2020-07-22 10:02:45 +0100

    added favicon

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
 	res.send("Welcome to home page baby");
 });
 
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
 //make server listen on certain port
 app.listen(port, () => {
 	console.log("Wazzzaaaappppp");
